f5ec0709dd00a0c42d0f106d73a783f80c5998a9
IahmadDev <ahmedusama723@gmail.com>
2020-05-13 10:28:10 +0500

    📦: Initial commit

---
+++ src/Crypto.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Crypto {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your text to encrypt: ");
+        String text = input.nextLine();
+        encryptedText(text);
+
+    }
+
+    public static void normalizeText(String text)
+    {
+
+    }
+
+    public static void obify(String text)
+    {
+
+    }
+
+    public static void ceasarify(String text)
+    {
+
+    }
+
+    public static void groupify(String text)
+    {
+
+    }
+
+    public static void encryptedText(String text)
+    {
+
+    }
+}
